46f846db06c33d826fd82c5ec8d5c589e07d684d
Francesco D'Amico <francesco.damico@eng.it>
2023-01-20 12:45:53 +0100

    first commit

---
+++ models/NbaPlayer.py
@@ -0,0 +1,41 @@
+from dataclasses import dataclass
+from functools import total_ordering
+
+@dataclass
+@total_ordering
+class NbaPlayer:
+    slug: str
+    team: str
+    latestThreeFixtureStats: int
+    latestFiveFixtureStats: int
+    tenGameAverage: int
+    latestThreeFixtureStatsDiff: int
+    latestFiveFixtureStatsDiff: int
+
+    def __init__(self, slug: str, team: str, latestThreeFixtureStats: int, latestFiveFixturesStats: int, tenGameAverage: int):
+        self.slug = slug
+        self.team = team
+        self.latestThreeFixtureStats = latestThreeFixtureStats
+        self.latestFiveFixtureStats = latestFiveFixturesStats
+        self.tenGameAverage = tenGameAverage
+        self.latestThreeFixtureStatsDiff = self.__performanceLastThreeDiff()
+        self.latestFiveFixtureStatsDiff = self.__performanceLastFiveDiff()
+
+    def __performanceLastThreeDiff(self) -> int:
+        return self.latestThreeFixtureStats - self.tenGameAverage
+        
+    def __performanceLastFiveDiff(self) -> int:
+        return self.latestFiveFixtureStats - self.tenGameAverage
+
+    def _is_valid_operand(self, other):
+        return hasattr(other, "latestThreeFixtureStatsDiff")
+
+    def __eq__(self, other):
+        if not self._is_valid_operand(other):
+            return NotImplemented
+        return self.slug == other.slug
+
+    def __gt__(self, other):
+        if not self._is_valid_operand(other):
+            return NotImplemented
+        return self.latestThreeFixtureStatsDiff > other.latestThreeFixtureStatsDiff
